caea3e7e246734412866d1864287286b333c4810
UcupPristel <rasyid.aulia@gmail.com>
2025-06-20 08:26:33 +0700

    Initial upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from Node.js app in Kubernetes!'));
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
