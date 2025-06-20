addd039cc3a0895a0375f3c6fdc9b52b17562e40
UcupPristel <rasyid.aulia@gmail.com>
2025-06-20 09:15:00 +0700

    Update app.js to version 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('Hello World ver 2'));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
