22a400fbfa3b223be7964fde5943281f9b391559
UcupPristel <rasyid.aulia@gmail.com>
2025-06-20 09:08:21 +0700

    Update app.js content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Node.js app in Kubernetes!'));
+app.get('/', (req, res) => res.send('Hello World'));
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
